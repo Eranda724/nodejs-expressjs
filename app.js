5776f3a5a1d9f5d0cf5b2fe7440fb6831a5de2a2
Eranda724 <erandajayasinghe724@gmail.com>
2025-01-18 14:27:35 +0530

    add folders

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Hello');
+const tut = require('./tut');
+
+//1
+//console.log(tut);
+//console.log(tut(10, 20));
+
+//2
+console.log(tut.sum(10, 20));
+console.log(tut.PI);
+console.log(new tut.SomeMathObject());
