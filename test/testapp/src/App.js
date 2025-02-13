a3940a654d435c1f5dc8d6720e2a65342647c22d
Eranda724 <erandajayasinghe724@gmail.com>
2025-02-13 20:44:53 +0530

    v1

---
+++ test/testapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome</h1>
+        <button className='user'>Click Me</button>
+        <p>Test App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
